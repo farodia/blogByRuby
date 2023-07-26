0b328b198431bb942399a8946ea68b0edf8b5828
farodia <2267067686@qq.com>
2023-07-26 10:23:52 -0500

    feat: add the edit view and action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,8 +2,9 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
   def create
-    @article = Article.new(params.require(:article).permit(:title, :text))
+    @article = Article.new(article_params)
 
     if @article.save
       redirect_to @article
@@ -17,12 +18,28 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-
+    @article = Article.find(params[:id])
   end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to article_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
 
     redirect_to new_article_path, status: :see_other
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
